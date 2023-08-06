5934fccf72b36c2e0e507ab88ae3b5a23771844b
pkoziel1 <74543639+pkoziel1@users.noreply.github.com>
2023-08-06 21:07:38 +0200

    Character generator (#8)
    * new file created
    
    * added remove_background function
    
    * upgraded remove_background function
    
    * added choose_character function
    
    * added characters png
    
    * minor change
    
    * added utlis file
    
    * moved function to utils
    
    * Update historyki_roblox/character_generator.py
    
    Co-authored-by: Vatraz <grubiszko@gmail.com>
    
    ---------
    
    Co-authored-by: Vatraz <grubiszko@gmail.com>

---
+++ historyki_roblox/utils.py
@@ -0,0 +1,17 @@
+import os
+
+from PIL import Image
+from rembg import remove
+
+
+def remove_background():
+    list_of_files = os.listdir('C:/Users/Janek/postacki/')
+    name_of_file = 0
+
+    for file in list_of_files:
+        name_of_file += 1
+        input_path = 'C:/Users/Janek/postacki/' + file
+        input_image = Image.open(input_path)
+        output_image = remove(input_image)
+        output_path = 'C:/Users/Janek/postacki_png/' + str(name_of_file) + '.png'
+        output_image.save(output_path)
