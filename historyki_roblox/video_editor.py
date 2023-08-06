72c46d2496042d40e0986d739c1b11851a3590e8
pkoziel1 <kozielec.paw@gmail.com>
2023-08-06 21:53:31 +0200

    finish the day

--- historyki_roblox/video_editor.py
+++ historyki_roblox/video_editor.py
@@ -4,14 +4,17 @@ ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
 class VideoGenerator:
     def edit_clip(self):
-# Load myHolidays.mp4 and select the subclip 00:00:50 - 00:00:60
+        ...
+
+
+# Load video and select the subclip
 clip = VideoFileClip(f'{ROOT_PATH}/data/videos/what.mp4').subclip(2,3)
 
 # Generate a text clip. You can customize the font, color, etc.
 txt_clip = TextClip("What?",fontsize=70,color='white')
 
 # Say that you want it to appear 10s at the center of the screen
-txt_clip = txt_clip.set_pos('center').set_duration(1)
+txt_clip = txt_clip.set_position(("center","top"))
 
 # Overlay the text clip on the first video clip
 video = CompositeVideoClip([clip, txt_clip])
