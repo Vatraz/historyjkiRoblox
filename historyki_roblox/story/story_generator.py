9111101d7f93ece1b441624fd92d4f4bf031506d
vatraz <grubiszko@gmail.com>
2023-08-06 21:33:36 +0200

    story generator

---
+++ historyki_roblox/story/story_generator.py
@@ -0,0 +1,32 @@
+import json
+import random
+from typing import Optional
+
+from historyki_roblox.gpt_relayer import GtpRelayer
+from historyki_roblox.story.story import Story
+from historyki_roblox.story.story_parser import GptStoryParser
+
+
+class StoryGenerator:
+    def __init__(self, gpt_relayer: Optional[GtpRelayer] = None):
+        self.gpt_relayer = gpt_relayer or GtpRelayer()
+        self.gpt_story_parser = GptStoryParser()
+        self.prompts_data = self._load_prompts_data()
+
+    def _load_prompts_data(self):
+        with open("data/stories/prompts.json") as fp:
+            prompts = json.load(fp)
+        return prompts
+
+    def generate_story_predefined(self) -> Story:
+        """
+        Generate a story from a random predefined prompt using chat GPT
+        """
+        prompt = random.choice(self.prompts_data["prompts_basic"])
+        raw_story = self.gpt_relayer.simply_ask(prompt)
+        story = self.gpt_story_parser.parse_raw_story(raw_story)
+        return story
+
+
+if __name__ == "__main__":
+    StoryGenerator().generate_story_predefined()
