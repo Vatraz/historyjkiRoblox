857a6b333950f7bc83be5db8f45bebc8440268bc
pkoziel1 <74543639+pkoziel1@users.noreply.github.com>
2023-08-06 21:04:16 +0200

    Update historyki_roblox/character_generator.py
    Co-authored-by: Vatraz <grubiszko@gmail.com>

--- historyki_roblox/character_generator.py
+++ historyki_roblox/character_generator.py
@@ -5,7 +5,7 @@ ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
 
 class CharacterGenerator:
-    def choose_character(self, requested_gender: str):
+    def choose_character(self, requested_gender: str) -> str:
         chosen_gender = requested_gender
         list_of_characters = os.listdir(f'{ROOT_PATH}/data/characters')
         return random.choice([char for char in list_of_characters if chosen_gender in char])
