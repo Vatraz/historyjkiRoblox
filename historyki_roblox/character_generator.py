262ccf61ed64ab6501d9a64b38a2fdc43a8b091f
FilipK0walewski <66330106+FilipK0walewski@users.noreply.github.com>
2023-08-13 19:06:53 +0200

    1 find a way to generate random oskareks (#13)
    * bing download
    
    * Random character generator
    
    * oskarek generator, openai
    
    ---------
    
    Co-authored-by: filip <filip@email.com>

--- historyki_roblox/character_generator.py
+++ historyki_roblox/character_generator.py
@@ -1,11 +1,42 @@
+import json
 import os
 import random
+from typing import NamedTuple, Optional
+
+from historyki_roblox.oskareks_generator import OskarekGenerator
 
 ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
 
+class Character(NamedTuple):
+    name: str
+    gender: str
+    voice: str
+    image: str
+    roblox_character: str
+
+
 class CharacterGenerator:
-    def choose_character(self, requested_gender: str) -> str:
-        chosen_gender = requested_gender
+    def __init__(self, oskarek_generator: Optional[OskarekGenerator] = None):
+        self.oskarek_generator = oskarek_generator or OskarekGenerator()
+        self.voices_data = self._load_voices()
+
+    def _load_voices(self) -> list:
+        with open('data/voices/voices.json', 'r') as f:
+            voices = json.load(f)
+        return voices['voices']
+
+    def choose_voice(self, gender: str) -> str:
+        return random.choice(tuple(filter(lambda x: x['ssmlGender'] == gender, self.voices_data)))['name']
+
+    def choose_roblox_character(self, requested_gender: str) -> str:
+        chosen_gender = 'f' if requested_gender == 'FEMALE' else 'm'
         list_of_characters = os.listdir(f'{ROOT_PATH}/data/characters')
         return random.choice([char for char in list_of_characters if chosen_gender in char])
+
+    def generate_random_character(self, name: str) -> Character:
+        gender = 'FEMALE' if name[-1] == 'a' else 'MALE'
+        voice = self.choose_voice(gender)
+        image = self.oskarek_generator.get_oskarek_from_openai(gender)
+        roblox_character = self.choose_roblox_character(gender)
+        return Character(name=name, gender=gender, voice=voice, roblox_character=roblox_character, image=image)
---
+++ historyki_roblox/oskarek_generator.py
@@ -0,0 +1,56 @@
+import os
+import json
+import random
+import requests
+import uuid
+
+from PIL import Image
+from io import BytesIO
+from lxml.html import fromstring
+from typing import Optional
+
+from historyki_roblox.gpt_relayer import GtpRelayer
+
+ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
+
+
+class OskarekGenerator:
+
+    def __init__(self, gpt_relayer: Optional[GtpRelayer] = None):
+        self.gpt_relayer = gpt_relayer or GtpRelayer()
+        self.face_descriptions_data = self._load_face_descriptions_data()
+
+    def _load_face_descriptions_data(self) -> dict:
+        with open('data/oskareks/descriptions.json', 'r') as f:
+            descriptions = json.load(f)
+        return descriptions
+
+    def get_oskarek_from_openai(self, gender):
+        description = random.choice(self.face_descriptions_data[gender])
+        image_url = self.gpt_relayer.generate_image(description)
+        response = requests.get(image_url)
+        image_data = BytesIO(response.content)
+        image = Image.open(image_data)
+        image_path = f'{ROOT_PATH}/output/oskareks/{str(uuid.uuid4())}.{image.format}'
+        image.save(image_path)
+        return image_path
+
+    def get_oskarek_from_bing(self, gender: str) -> str:
+        search = 'instaboy' if gender == 'MALE' else 'instagirl'
+        response = requests.get('https://www.bing.com/images/search', params={'q': search})
+        html = fromstring(response.text)
+
+        tmp = 'm' if gender == 'MALE' else 'f'
+        images = html.xpath(f"//*[contains(concat(' ', normalize-space(@class), ' '), ' mimg ')]")
+        for c, i in enumerate(images):
+            src = i.get('src')
+            if src is None:
+                src = i.get('data-src')
+            if src is None:
+                continue
+            image_response = requests.get(src)
+            image_data = BytesIO(image_response.content)
+            image = Image.open(image_data)
+            image_format = image.format
+            image_name = f'{tmp}{c}_bing.{image_format}'
+            image.save(f'{ROOT_PATH}/data/oskareks/{image_name}')
