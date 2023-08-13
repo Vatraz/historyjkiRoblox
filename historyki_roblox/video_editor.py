f7a7d7d5eb13d0e5bf0b883ef6779d2005322083
pkoziel1 <kozielec.paw@gmail.com>
2023-08-13 17:59:47 +0200

    filled load_background_clip

--- historyki_roblox/video_editor.py
+++ historyki_roblox/video_editor.py
@@ -5,40 +5,45 @@ ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
 
 class VideoGenerator:
-    def create_clip(self):
+
+    def generate_video(self, scenario):
         ...
-        #load background clip with and choose the length
-        #add characters
-        #add subtitles
-
-    def load_background_clip(self, clip_length):
-        return VideoFileClip(random.choice(os.listdir(f'{ROOT_PATH}/data/videos/'))).set_duration(clip_length)
-
-    def add_subtitles(self, text, corner):
-        ...
-
-    def add_character(self, picked_name):
-        ...
-
-    def add_name(self):
-        ...
-
-    def add_oskarek(self):
-        ...
-
-# Load video and select the subclip
-clip = VideoFileClip(f'{ROOT_PATH}/data/videos/what.mp4').subclip(2,3)
-
-# Generate a text clip. You can customize the font, color, etc.
-txt_clip = TextClip("What?",fontsize=70,color='white')
-
-# Say that you want it to appear 10s at the center of the screen
-txt_clip = txt_clip.set_position(("center","top"))
-
-# Overlay the text clip on the first video clip
-video = CompositeVideoClip([clip, txt_clip])
-
-# Write the result to a file (many options available !)
-video.write_videofile(f'{ROOT_PATH}/data/videos/what_edited.mp4')
-
-print('ready')
+        for text_line in scenario:
+            ...
+#     def create_clip(self):
+#         ...
+#         #load background clip with and choose the length
+#         #add characters
+#         #add subtitles
+#
+#     def load_background_clip(self, clip_length):
+#         return VideoFileClip(random.choice(os.listdir(f'{ROOT_PATH}/data/videos/'))).set_duration(clip_length)
+#
+#     def add_subtitles(self, text, corner):
+#         ...
+#
+#     def add_character(self, picked_name):
+#         ...
+#
+#     def add_name(self):
+#         ...
+#
+#     def add_oskarek(self):
+#         ...
+#
+# # Load video and select the subclip
+# clip = VideoFileClip(f'{ROOT_PATH}/data/videos/what.mp4').subclip(2,3)
+#
+# # Generate a text clip. You can customize the font, color, etc.
+# txt_clip = TextClip("What?",fontsize=70,color='white')
+#
+# # Say that you want it to appear 10s at the center of the screen
+# txt_clip = txt_clip.set_position(("center","top"))
+#
+# # Overlay the text clip on the first video clip
+# video = CompositeVideoClip([clip, txt_clip]).cutout
+#
+# # Write the result to a file (many options available !)
+# video.write_videofile(f'{ROOT_PATH}/data/videos/what_edited.mp4')
+#
+# print('ready')
