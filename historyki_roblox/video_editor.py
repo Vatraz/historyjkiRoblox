9ddedc6c7fafac944d8c2a1b952252e417fabca1
filip <filipkowalewski00@gmail.com>
2023-08-18 19:51:03 +0200

    video generation

--- historyki_roblox/video_editor.py
+++ historyki_roblox/video_editor.py
@@ -1,9 +1,12 @@
 import os
 import random
 
-from moviepy.editor import *
+import numpy as np
+import moviepy.editor as mpv
 
-from historyki_roblox.character_generator import CharacterGenerator
+from typing import NamedTuple
+
+from historyki_roblox.character_generator import Character, CharacterGenerator
 from historyki_roblox.voice_generator import VoiceGenerator
 from historyki_roblox.story.story import Story, Dialogue
 from historyki_roblox.story.story_parser import GptStoryParser
@@ -11,97 +14,136 @@ from historyki_roblox.story.story_parser import GptStoryParser
 ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
 
+class Actor:
+    def __init__(self, character: Character, face_image: mpv.ImageClip, roblox_image: mpv.ImageClip, side: str):
+        self.character = character
+        self.face_image = face_image
+        self.roblox_image = roblox_image
+        self.side = side
+        self.is_online = False
+        self.is_camera_on = False
+
+    def speak(self, text: str, font: str) -> mpv.TextClip:
+        current_image = self.face_image if self.is_camera_on is True else self.roblox_image
+        image_x, image_y = current_image.pos(0)
+        image_width, image_height = current_image.size
+
+        text_clip = mpv.TextClip(text, fontsize=40, color='yellow', stroke_color='black', stroke_width=1.65, font=font, method='caption', align=self.side, size=(500, None))
+        text_width, text_height = text_clip.size
+
+        text_x, text_y = 0, image_y + image_height * .5 - text_height * .5
+        if self.side == 'West':
+            text_x = image_x + image_width
+        elif self.side == 'East':
+            text_x = image_x - text_width
+
+        text_clip = text_clip.set_position((text_x, text_y))
+        return text_clip
+
+    def join_room(self):
+        self.roblox_image.set_opacity(1)
+        self.is_online = True
+    
+    def leave_room(self):
+        self.roblox_image.set_opacity(0)
+        self.is_online = False
+
+    def turn_on_camera(self):
+        self.roblox_image.set_opacity(0)
+        self.face_image.set_opacity(1)
+        self.is_camera_on = True
+
+    def turn_off_camera(self):
+        self.roblox_image.set_opacity(1)
+        self.face_image.set_opacity(0)
+        self.is_camera_on = False
+
+
+class ActorGenerator:
+    def __init__(self, clip_width, clip_height):
+        self.clip_width = clip_width
+        self.clip_height = clip_height
+
+    def create_image(self, image_source_path: str, position_number: int) -> mpv.ImageClip:
+        if image_source_path is None:
+            return None
+
+        image_clip = mpv.ImageClip(image_source_path)
+
+        image_width, image_height = image_clip.size
+
+        image_x, image_y = 0, 0
+        if position_number == 0:
+            image_x = 0
+            image_y = self.clip_height * .25 - image_height * .5
+        elif position_number == 1:
+            image_x = self.clip_width - image_width
+            image_y = self.clip_height * .25 - image_height * .5
+        elif position_number == 2:
+            image_x = 0
+            image_y = self.clip_height * .75 - image_height * .5
+        elif position_number == 3:
+            image_x = self.clip_width - image_width
+            image_y = self.clip_height * .75 - image_height * .5
+
+        image_clip = image_clip.set_position((image_x, image_y))
+        return image_clip
+
+    def generate_actor(self, character: Character, position_number: int) -> Actor:
+        roblox_image = self.create_image(character.roblox_image_path, position_number)
+        face_image = self.create_image(character.face_image_path, position_number)
+        side = 'West' if position_number % 2 == 0 else 'East'
+        return Actor(character, face_image, roblox_image, side)
+
+
 class VideoGenerator:
 
     def __init__(self):
         self.voice_generator = VoiceGenerator()
         self.character_generator = CharacterGenerator()
-        self.positions = {'top_left': (0, 0), 'top_right': (500, 0), 'bottom_left': (0, 500), 'bottom_right': (500, 500)}
+        self.font = '/usr/share/fonts/adobe-source-code-pro/SourceCodePro-Semibold.otf'
+        self.pause_duration = .5
 
-    def generate_video(self, story: Story, used_clip):
-        characters, characters_positions, available_positions = {}, {}, list(self.positions.keys())
-        for name in story.actors:
-            position = random.choice(available_positions)
-            characters[name] = self.character_generator.generate_random_character(name)
-            characters_positions[name] = position
-            available_positions.remove(position)
+    def generate_video(self, story: Story, clip: mpv.VideoFileClip):
+        clip_width, clip_height = clip.size
+        actor_generator = ActorGenerator(clip_width, clip_height)
 
-        return
-        timestamp = 0
-        added_text = []
-        added_image = []
+        actors = {}
+        for c, name in enumerate(story.actors):
+            generated_character = self.character_generator.generate_random_character(name)
+            actors[name] = actor_generator.generate_actor(generated_character, c)
+
+        clip_duration, added_text, added_sound = 0, [], []
         for scenario_element in story.scenario:
-            text_line = scenario_element.content
-            speech = self.voice_generator.synthesize(text_line)
-            # speech.duration
-            # speech.source_path
+            actor = actors[scenario_element.actor]
+            speech = self.voice_generator.synthesize(scenario_element.content, voice=actor.character.voice)
 
-            text_clip = TextClip(text_line, font='Arial', fontsize=12, color='white')
-            text_clip.set_position(self.position(scenario_element.speaker))
-            text_clip.set_duration
-            character = scenario_element.actor
+            audio_clip = mpv.AudioFileClip(speech.source_path)
+            silent_audio = mpv.AudioClip(make_frame=lambda t: [0], duration=self.pause_duration)
+            added_sound.append(audio_clip)
+            added_sound.append(silent_audio)
 
-            added_text.append(TextClip(text_line, font='Arial', fontsize=12, color='white')
-                              .set_position("center")
-                              .set_duration(len(text_line)/20).set_start(timestamp))
+            text_clip = actor.speak(scenario_element.content, self.font)
+            text_clip = text_clip.set_start(clip_duration)
+            text_clip = text_clip.set_duration(audio_clip.duration)
+            added_text.append(text_clip)
 
-            added_image.append(ImageClip(character)
-                               .set_position("top")
-                               .set_duration(len(text_line)/20).set_start(timestamp))
-            timestamp += (len(text_line)/20 + 0.5)
-        return CompositeVideoClip([used_clip] + added_text + added_image)
+            clip_duration += (speech.duration + self.pause_duration)
 
-    def position(self, character):
-        ...
-#     def create_clip(self):
-#         ...
-#         #load background clip with and choose the length
-#         #add characters
-#         #add subtitles
-#
-#     def load_background_clip(self, clip_length):
-#         return VideoFileClip(random.choice(os.listdir(f'{ROOT_PATH}/data/videos/'))).set_duration(clip_length)
-#
-#     def add_subtitles(self, text, corner):
-#         ...
-#
-#     def add_character(self, picked_name):
-#         ...
-#
-#     def add_name(self):
-#         ...
-#
-#     def add_oskarek(self):
-#         ...
-#
-# # Load video and select the subclip
-# clip = VideoFileClip(f'{ROOT_PATH}/data/videos/what.mp4').subclip(2,3)
-#
-# # Generate a text clip. You can customize the font, color, etc.
-# txt_clip = TextClip("What?",fontsize=70,color='white')
-#
-# # Say that you want it to appear 10s at the center of the screen
-# txt_clip = txt_clip.set_position(("center","top"))
-#
-# # Overlay the text clip on the first video clip
-# video = CompositeVideoClip([clip, txt_clip]).cutout
-#
-# # Write the result to a file (many options available !)
-# video.write_videofile(f'{ROOT_PATH}/data/videos/what_edited.mp4')
-#
-# print('ready')
+        added_images = []
+        for _, actor in actors.items():
+            image_clip = actor.roblox_image
+            image_clip = image_clip.set_duration(clip_duration)
+            added_images.append(image_clip)
 
+        combined_audio = mpv.concatenate_audioclips(added_sound)
 
-if __name__ == '__main__':
-    size = (1000, 500)
-    duration = 10
-    color = (0, 0, 0)
-    output = f'{ROOT_PATH}/data/videos/color.mp4'
-    fps = 25
-    clip = ColorClip(size, color, duration=duration)
+        video_loop = []
+        for _ in range(int(combined_audio.duration / clip.duration) + 1):
+            video_loop.append(clip)
 
-    input = "".join(open("data/stories/test_data/0.txt").readlines())
-    story = GptStoryParser().parse_raw_story(input)
-
-    video = VideoGenerator().generate_video(story, clip)
-    video.write_videofile(output, fps=fps)
+        concatenated_loop = mpv.concatenate_videoclips(video_loop)
+        video = mpv.CompositeVideoClip([concatenated_loop] + added_images + added_text)
+        video = video.set_duration(combined_audio.duration)
+        video = video.set_audio(combined_audio)
+        return video
