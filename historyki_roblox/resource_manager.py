7f326c29485d422f0f9c52b609999ee718e38e0c
pkoziel1 <kozielec.paw@gmail.com>
2023-08-28 13:04:47 +0200

    added oskarek saver and getter

--- historyki_roblox/resource_manager.py
+++ historyki_roblox/resource_manager.py
@@ -10,10 +10,12 @@ class ResourceManager:
 
     def __init__(self):
         self.root_path = os.path.dirname(os.path.dirname(__file__))
-        self.resource_manager = ResourceManager()
 
     def get_list_of_characters(self):
         return os.listdir(f'{self.root_path}/data/characters')
+
+    def get_list_of_oskareks(self):
+        return os.listdir(f'{self.root_path}/output/oskareks')
 
     def get_roblox_character(self, requested_gender: str) -> str:
         chosen_gender = 'f' if requested_gender == 'FEMALE' else 'm'
@@ -36,7 +38,12 @@ class ResourceManager:
         )
 
     def save_oskarek_image(self, image: Image, image_name: str):
-        return image.save(f'{self.root_path}/data/oskareks/{image_name}')
+        return image.save(f'{self.root_path}/output/oskareks/{image_name}')
+
+    def get_oskarek_image(self, requested_gender: str) -> str:
+        chosen_gender = 'f' if requested_gender == 'FEMALE' else 'm'
+        chosen = random.choice([char for char in self.get_list_of_oskareks() if chosen_gender == char[0]])
+        return f'{self.root_path}/output/oskareks/{chosen}'
 
     def get_voices(self) -> list:
         with open(f'{self.root_path}/data/voices/voices.json', 'r') as f:
@@ -44,4 +51,4 @@ class ResourceManager:
         return voices['voices']
 
 
-print(ResourceManager().get_list_of_characters())
+print(ResourceManager().get_oskarek_image('FEMALE'))
