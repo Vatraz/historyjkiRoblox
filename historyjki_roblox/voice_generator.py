0265cc15d76515ee994e2af9e3aa7323fb1a819d
Vatraz <grubiszko@gmail.com>
2023-10-10 21:15:59 +0200

    fix exception type in VoiceGenerator

--- historyjki_roblox/voice_generator.py
+++ historyjki_roblox/voice_generator.py
@@ -55,7 +55,7 @@ class VoiceGenerator:
             f"{self.base_url}/text:synthesize", params=params, json=body
         )
         if response.status_code != 200:
-            raise VoiceGenerator(
+            raise VoiceGeneratorException(
                 f"response status code: {response.status_code}\n{response.text}"
             )
 
