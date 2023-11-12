dcb4ad3d236193bc0d6aa8b38f640f93aee5d660
pkoziel1 <kozielec.paw@gmail.com>
2023-11-12 17:53:45 +0100

    update

--- historyjki_roblox/story/drama_story.py
+++ historyjki_roblox/story/drama_story.py
@@ -1,3 +1,5 @@
+import re
+
 from historyjki_roblox.story.story import Dialogue, Didascalia, Event, Scenario, Story
 from historyjki_roblox.story.story_parser_base import StoryParserBase
 
@@ -24,8 +26,47 @@ class DramaStoryParser(StoryParserBase):
         story_lines = [line for line in story_lines if line]
         return story_lines
 
+    def _is_line_dialogue(self, line: str) -> bool:
+        return not line.startswith(":") and ":" in line
+
+    def _is_line_event(self, line: str) -> bool:
+        return "=>" in line
+
+    def _parse_line_to_dialogue(self, line: str) -> Dialogue:
+        if "[" in line:
+            emotion_match = re.search("\[.*\\]", line)
+            content_raw = line[emotion_match.end() :]
+            name_raw = line[: emotion_match.start()]
+
+            emotion = emotion_match.group()[1:-1]
+            name = name_raw.strip()
+            content = " ".join(content_raw.split()[1:])
+        else:
+            name, content = line[: line.index(":")], line[line.index(":") + 1 :]
+            name, content = name.strip(), content.strip()
+            emotion = None
+
+        return Dialogue(content=content, actor=name, emotion=emotion)
+
+    def _parse_line_to_event(self, line: str) -> Event:
+        name, action = line.split("=>")
+        return Event(actor=name.strip(), action=action.strip())
+
+    def _parse_line_to_didascalia(self, line: str) -> Didascalia:
+        return Didascalia(content=line.strip())
+
+    def _get_actors_from_scenario(self, scenario: Scenario) -> list[str]:
+        actors = list(
+            set(
+                scenario_element.actor
+                for scenario_element in scenario
+                if type(scenario_element) == Dialogue
+            )
+        )
+        return actors
+
 
 if __name__ == "__main__":
-    input = "".join(open("data/stories/test_data/3.txt").readlines())
+    input = "".join(open("data/stories/test_data/dziady.txt", encoding="utf8").readlines())
     parsed_story = DramaStoryParser().parse_raw_story(input)
     print("done")
