aa3772e9147b063be5aa298066ff8178c86ca5c6
Vatraz <grubiszko@gmail.com>
2023-11-12 14:15:56 +0100

    raise -> pass

--- historyjki_roblox/story/story_parser_base.py
+++ historyjki_roblox/story/story_parser_base.py
@@ -6,4 +6,4 @@ from historyjki_roblox.story.story import Story
 class StoryParserBase(ABC):
     @abstractmethod
     def parse_raw_story(self, raw_story: str) -> Story:
-        raise
+        pass
