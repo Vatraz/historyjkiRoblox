caedada90b0cac1e85250439bff3eef5b61a94ae
pkoziel1 <kozielec.paw@gmail.com>
2023-12-03 16:44:49 +0100

    prawie gotowe

--- historyjki_roblox/story/drama_story.py
+++ historyjki_roblox/story/drama_story.py
@@ -6,64 +6,44 @@ from historyjki_roblox.story.story_parser_base import StoryParserBase
 
 class DramaStoryParser(StoryParserBase):
     def parse_raw_story(self, raw_story: str) -> Story:
-        story_lines = self._extract_stroy_lines(raw_story)
+        story_lines = self._extract_story_lines(raw_story)
 
         scenario = []
+        actors = []
+        current_actor = None
         for line in story_lines:
-            if self._is_line_dialogue(line):
-                scenario.append(self._parse_line_to_dialogue(line))
+            if self._is_line_actor(line):
+                if line not in actors:
+                    actors.append(line)
+                current_actor = line
             elif self._is_line_event(line):
-                scenario.append(self._parse_line_to_event(line))
+                pass
             else:
-                scenario.append(self._parse_line_to_didascalia(line))
-
-        actors = self._get_actors_from_scenario(scenario)
+                scenario.append(self._parse_line_to_dialogue(line, current_actor))
 
         return Story(scenario=scenario, actors=actors)
 
-    def _extract_stroy_lines(self, raw_story: str) -> list[str]:
+    def _extract_story_lines(self, raw_story: str) -> list[str]:
         story_lines = raw_story.split("\n")
         story_lines = [line for line in story_lines if line]
         return story_lines
 
+    def _is_line_actor(self, line: str) -> bool:
+        return line.isupper()
+
     def _is_line_dialogue(self, line: str) -> bool:
-        return not line.startswith(":") and ":" in line
+        dialogue_line = line.strip()
+        return dialogue_line[0].isupper()
 
     def _is_line_event(self, line: str) -> bool:
-        return "=>" in line
+        return line.startswith("/")
 
-    def _parse_line_to_dialogue(self, line: str) -> Dialogue:
-        if "[" in line:
-            emotion_match = re.search("\[.*\\]", line)
-            content_raw = line[emotion_match.end() :]
-            name_raw = line[: emotion_match.start()]
-
-            emotion = emotion_match.group()[1:-1]
-            name = name_raw.strip()
-            content = " ".join(content_raw.split()[1:])
-        else:
-            name, content = line[: line.index(":")], line[line.index(":") + 1 :]
-            name, content = name.strip(), content.strip()
-            emotion = None
+    def _parse_line_to_dialogue(self, line: str, actor: str) -> Dialogue:
+        content = line.strip()
+        name = actor
+        emotion = None
 
         return Dialogue(content=content, actor=name, emotion=emotion)
-
-    def _parse_line_to_event(self, line: str) -> Event:
-        name, action = line.split("=>")
-        return Event(actor=name.strip(), action=action.strip())
-
-    def _parse_line_to_didascalia(self, line: str) -> Didascalia:
-        return Didascalia(content=line.strip())
-
-    def _get_actors_from_scenario(self, scenario: Scenario) -> list[str]:
-        actors = list(
-            set(
-                scenario_element.actor
-                for scenario_element in scenario
-                if type(scenario_element) == Dialogue
-            )
-        )
-        return actors
 
 
 if __name__ == "__main__":
